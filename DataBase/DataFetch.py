24e159e1513e2607b2a09290a0eacfc7716bb882
Yash goel <49948928+root-yash@users.noreply.github.com>
2022-04-09 23:10:27 +0530

    Update DataFetch.py

--- DataBase/DataFetch.py
+++ DataBase/DataFetch.py
@@ -9,5 +9,5 @@ def fetch(index):
     )
     db = client["SnakeDetails"]
     collection = db["SnakeDataset"]
-    data = collection.find_one({'idx': index+1}, {"_id": 0, "idx": 0, "Genus": 0})
+    data = collection.find_one({'idx': str(int(index)+1)}, {"_id": 0, "idx": 0, "Genus": 0})
     return data
